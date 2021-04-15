8801298b35a67d61da98dc67b909516311fe6748
cindyderin27 <61082408+cindyderin27@users.noreply.github.com>
2021-04-15 08:16:40 +0200

    Ajoutez des fichiers projet.

---
+++ ApiApplicationCC/Controllers/ProprietaireController.cs
@@ -0,0 +1,67 @@
+﻿using ApiApplicationCC.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ApiApplicationCC.Controllers
+{
+    public class ProprietaireController : ApiController
+    {
+        ProprieteEntities db = new ProprieteEntities();
+
+        [HttpGet]
+        public IHttpActionResult GetListProprietaire()
+        {
+            try
+            {
+                 db.Configuration.ProxyCreationEnabled = false;
+                var proprietaires = db.Proprietaires.OrderByDescending(x => x.DateCreation).ToList();
+                return Ok(proprietaires);
+
+            }
+
+
+            catch (Exception ex)
+
+            {
+                return BadRequest(ex.Message);
+            }
+
+        }
+
+        [HttpGet]
+
+        public IHttpActionResult GetProprietaire(int id)
+        {
+            try
+            {
+
+                db.Configuration.ProxyCreationEnabled = false;
+                var proprietaire = db.Proprietaires.Find(id);
+
+                return Ok(proprietaire);
+
+            }
+            catch (DbUpdateException ex)
+            {
+                var exception = ex.InnerException?.InnerException as SqlException;
+                return BadRequest(exception?.Message);
+
+            }
+
+            catch (Exception ex)
+
+            {
+                return BadRequest(ex.Message);
+            }
+
+
+        }
+
+    }
+}
